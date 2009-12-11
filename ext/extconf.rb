19fd51fc1cdabe899ca89216d3347392de10cf73
Aman Gupta <aman@tmm1.net>
2009-12-11 14:14:29 -0800

    Build yajl

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -7,6 +7,42 @@ if RUBY_VERSION >= "1.9"
 end
 
 require 'mkmf'
+require 'fileutils'
+
+CWD = File.expand_path(File.dirname(__FILE__))
+
+def sys(cmd)
+  puts "  -- #{cmd}"
+  unless ret = xsystem(cmd)
+    raise "#{cmd} failed, please report to gdb@tmm1.net with pastie.org link to #{CWD}/mkmf.log and #{CWD}/src/gdb-7.0/config.log"
+  end
+  ret
+end
+
+yajl = File.basename('yajl-1.0.7.tar.gz')
+dir = File.basename(yajl, '.tar.gz')
+
+unless File.exists?("#{CWD}/dst/lib/libyajl_s.a")
+  puts "(I'm about to compile yajl.. this will definitely take a while)"
+
+  Dir.chdir('src') do
+    FileUtils.rm_rf(dir) if File.exists?(dir)
+
+    sys("tar zxvf #{yajl}")
+    Dir.chdir(dir) do
+      sys("./configure --prefix=#{CWD}/dst/")
+      sys("make")
+      sys("make install")
+    end
+  end
+end
+
+$LIBPATH << "#{CWD}/dst/lib"
+$INCFLAGS << "-I#{CWD}/dst/include"
+
+unless have_library('yajl_s') and have_header('yajl/yajl_gen.h')
+  raise 'Yajl build failed'
+end
 
 def add_define(name)
   $defs.push("-D#{name}")
