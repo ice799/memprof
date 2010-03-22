c179f3e7fae1eb8b0131d2749e033ae0f69f96d3
Jake Douglas <jakecdouglas@gmail.com>
2010-03-21 22:56:06 -0700

    Rename usr2.rb to signal.rb and change to SIGINFO.

---
+++ lib/memprof/signal.rb
@@ -0,0 +1,16 @@
+begin
+  require File.expand_path('../../memprof', __FILE__)
+rescue LoadError
+  require File.expand_path('../../../ext/memprof', __FILE__)
+end
+
+Memprof.start
+old_handler = trap('INFO'){
+  pid = Process.pid
+  fork{
+    GC.start
+    Memprof.dump_all("/tmp/memprof-#{pid}-#{Time.now.to_i}.json")
+    exit!
+  }
+  old_handler.call if old_handler
+}
