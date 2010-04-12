9421415ddca3359b794cd317148425847e4629c1
Joe Damato <ice799@gmail.com>
2010-04-12 11:01:33 -0400

    I fail at git.

--- ext/util.h
+++ ext/util.h
@@ -4,7 +4,7 @@
 #if defined(_MEMPROF_DEBUG)
 #include <stdio.h>
 #define dbg_printf(...) do {\
-  fprintf(stderr, "%s:%s  ", __FILE__, __LINE__);\
+  fprintf(stderr, "%s:%d  ", __FILE__, __LINE__);\
   fprintf(stderr, __VA_ARGS__);\
   } while (0)
 #else
