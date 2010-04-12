b7f2e77c18fc5935dbc9535083be22924a1a5637
Joe Damato <ice799@gmail.com>
2010-04-12 10:54:56 -0400

    Add file and line numbers to debug print

--- ext/util.h
+++ ext/util.h
@@ -3,7 +3,10 @@
 
 #if defined(_MEMPROF_DEBUG)
 #include <stdio.h>
-#define dbg_printf(...) fprintf(stderr, __VA_ARGS__)
+#define dbg_printf(...) do {\
+  fprintf(stderr, "%s:%d  ", __FILE__, __LINE__);\
+  fprintf(stderr, __VA_ARGS__);\
+  } while (0)
 #else
 #define dbg_printf(...)
 #endif
